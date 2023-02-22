4dcd419b1a76869b0155bf5a870edf765fcf16af
Konstantin Komisarov <komissko1@yandex.ru>
2023-02-22 14:47:18 +0100

    GH

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App/>
-    </BrowserRouter>
-  </React.StrictMode>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
